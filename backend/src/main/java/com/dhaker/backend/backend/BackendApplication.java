7b27a7592092c9821d3941d6a9cfcc6463681f8b
SUAREZ REDONDO Rafael <rafael.suarez@soprasteria.com>
2018-04-30 22:50:06 +0200

    CRUD cars

--- backend/src/main/java/com/dhaker/backend/backend/BackendApplication.java
+++ backend/src/main/java/com/dhaker/backend/backend/BackendApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import java.util.stream.Stream;
 
-@EnableResourceServer
 @SpringBootApplication
 public class BackendApplication {
 
--- client/src/app/car-edit/car-edit.component.ts
+++ client/src/app/car-edit/car-edit.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarService } from '../shared/car/car.service';
+import { GiphyServiceService } from '../shared/giphy/giphy-service.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-car-edit',
   templateUrl: './car-edit.component.html',
   styleUrls: ['./car-edit.component.css']
 })
-export class CarEditComponent implements OnInit {
+export class CarEditComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  car: any = {};
+
+  sub: Subscription;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private carService: CarService,
+    private giphyService: GiphyServiceService) {
+  }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.carService.get(id).subscribe((car: any) => {
+          if (car) {
+            this.car = car;
+            this.car.href = car._links.self.href;
+            this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
+          } else {
+            console.log(`Car with id '${id}' not found, returning to list`);
+            this.gotoList();
+          }
+        });
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
+
+  gotoList() {
+    this.router.navigate(['/car-list']);
+  }
+
+  save(form: NgForm) {
+    this.carService.save(form).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+  remove(href) {
+    this.carService.remove(href).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
   }
 
 }
